2c6cd456a3d9a7557895e450aee731743f601dfa
PaulWells <paulwells12@gmail.com>
2013-07-01 19:38:34 -0400

    original commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PreWithMe::Application.config.secret_token = 'e4408c4a25c83f6d5e3bebe94a6aef377de9204a436cb984e40e11c459b487ca07b23973ec5929e0adbad125b36405fcb6341abb492ed289839cbdbc6a511f0a'
